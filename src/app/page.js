1dafa91c7a5be8c0e0c9e7a7583f6c316d66358b
Paulo Santos <paulo.santos@compasso.com.br>
2024-11-11 19:12:09 -0300

    Adiciona link para envio de medicamentos

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-
-import Write from './components/write';
-
 export default function Home() {
   return (
     <div>
       <h1>API ExpiryGuard</h1>
-      <Write/>
+      <a href="https://api-expiryguard.vercel.app/api"> API para envio de Medicamentos</a>
     </div>
   );
 }
